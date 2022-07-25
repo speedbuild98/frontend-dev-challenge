d798073dfe8ce1a675cf49f45aa1255cef507b39
speedbuild98 <lautaropectm98@gmail.com>
2022-07-25 08:57:10 -0300

    Added preloader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+//Dependencies
+import React, { useState, useEffect } from 'react';
+
 //Components
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
@@ -11,24 +14,50 @@ import Aside from "./components/Aside/Aside";
 //Styles
 import "./App.scss";
 
+//Images
+import loaderGif from '../src/assets/loader.gif'
+import logoMenu from '../src/assets/logoMenu.svg'
+import logoSmall from '../src/assets/favicon.svg'
+
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
+
+
   return (
-    <div className="App">
-      <Nav />
-      <Header />
-      <div className="mainContainer">
-      <Aside />
-        <div className="container">
-          <Profile />
-          <Content />
+    <div>
+      {loading ? (
+        <div id="loader">
+          <img src={logoSmall} alt="butterfly logo" />
+          <img src={logoMenu} alt="menu logo" />
+          <img src={loaderGif} alt="loader logo" />
         </div>
-      </div>
-      <Newsletter />
-      <FAQ />
-      <Footer />
+      ) : (
+        <div className="App">
+          <Nav />
+          <Header />
+          <div className="mainContainer">
+            <Aside />
+            <div className="container">
+              <Profile />
+              <Content />
+            </div>
+          </div>
+          <Newsletter />
+          <FAQ />
+          <Footer />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
